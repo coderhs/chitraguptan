9b0bb377cdf7d6c7707449a5d45cf5d2dc115259
Harisankar P S <mailme@hsps.in>
2022-07-27 22:09:07 -0700

    rails generate chitraguptan:migrations

---
+++ lib/generators/chitraguptan/migrations/migrations_generator.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails/generators/migration"
+
+module Chitraguptan
+  module Generators
+    class MigrationsGenerator < ::Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      source_root File.expand_path("templates", __dir__)
+      desc "Create Chitraguptan migrations"
+
+      def self.next_migration_number(_path)
+        if @prev_migration_nr
+          @prev_migration_nr += 1
+        else
+          @prev_migration_nr = Time.now.strftime("%Y%m%d%H%M%S").to_i
+        end
+
+        @prev_migration_nr.to_s
+      end
+
+      def copy_migrations
+        migration_template "create_chitraguptan_variables.rb",
+                           "db/migrate/create_chitraguptan_variables.rb",
+                           migration_version: migration_version
+      end
+
+      def migration_version
+        "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+      end
+    end
+  end
+end
